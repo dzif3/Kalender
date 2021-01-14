0f8dcfbd91964eb96a5ed7409a02705e98edc554
dzikra <dzikrahappy@gmail.com>
2021-01-14 11:10:50 +0700

    kalender

---
+++ app/src/main/java/com/example/calendar/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewDate;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewDate = findViewById(R.id.text_view_date);
+    }
+    public void getDate(View v) {
+        Calendar c = Calendar.getInstance();
+        Date today = c.getTime();
+        c.add(Calendar.DATE, 3);
+        c.add(Calendar.MONTH, 1);
+        c.add(Calendar.YEAR, 1);
+        Date future = c.getTime();
+        textViewDate.setText("Today: " + today + "\n" + "Future: " + future);
+    }
+}
